2ba1a22ccf9ff84da4ffbedb5915fc6b92058811
kyaba0612 <soutakawamura@icloud.com>
2020-09-22 03:20:37 +0900

    first commit

---
+++ Accelerometer/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  Accelerometer
+//
+//  Created by チップまん on 2020/09/21.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var accelerationX: Double!
+    
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let result: Double = fabs(accelerationX*100)
+        label.text = String(format: "%.1f ﾟ",result)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
